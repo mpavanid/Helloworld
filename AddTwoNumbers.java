9c0c5f879ed8010e7504121614aeab63fcc1541c
mpavanid <30336657+mpavanid@users.noreply.github.com>
2019-07-27 09:40:08 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,7 +2,7 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 10, sum;
+      int num1 = 5, num2 = 11, sum;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
